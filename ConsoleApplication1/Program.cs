8c23d94eb9e7cd4868248cf4dcf7fe742f8f823b
Tompot, Randy <Randy.Tompot@schwab.com>
2015-12-01 15:55:51 -0600

    Added second line

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hi Randy");
+            Console.WriteLine("Hi Lisa");
         }
     }
 }
